6e9729a4a4c113f4a5e706861f1ca22e054eb8cf
Petr Halinen <petr.halinen@tuni.fi>
2021-12-27 16:39:54 +0200

    Add connectionFunctions export & getAll function

--- connection.js
+++ connection.js
@@ -16,3 +16,25 @@ var config = {
  * Create connectionpool based on config
  */
 var pool = mysql.createPool(config);
+
+/**
+ * Functions used for to get data from database
+ */
+let connectionFunctions = {
+  getAll: () =>
+    new Promise((resolve, reject) => {
+      pool.getConnection((err, connection) => {
+        if (err) throw err;
+        var sql = `select * from Vocabulary`;
+        connection.query(sql, (error, result) => {
+          if (error) {
+            reject(error);
+          }
+          resolve(result);
+          connection.release();
+        });
+      });
+    }),
+};
+
+module.exports = connectionFunctions;
