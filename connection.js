f94bd411b30c0e15ccd8a1ac46055b9def3fbb60
Petr Halinen <petr.halinen@tuni.fi>
2021-12-27 16:40:47 +0200

    Add 'post' function

--- connection.js
+++ connection.js
@@ -35,6 +35,27 @@ let connectionFunctions = {
         });
       });
     }),
+
+  post: (body) =>
+    new Promise((resolve, reject) => {
+      pool.getConnection((err, connection) => {
+        if (err) throw err;
+        var sql = `INSERT INTO Vocabulary (fi_word, eng_word, sv_word, ru_word) values (${connection.escape(
+          body.fi_word
+        )}, ${connection.escape(body.eng_word)}, ${connection.escape(
+          body.sv_word
+        )}, ${connection.escape(body.ru_word)})`;
+        console.log(sql);
+        connection.query(sql, (error) => {
+          if (error) {
+            reject(error);
+          }
+          resolve(body);
+        });
+        connection.release();
+        if (err) throw err;
+      });
+    }),
 };
 
 module.exports = connectionFunctions;
