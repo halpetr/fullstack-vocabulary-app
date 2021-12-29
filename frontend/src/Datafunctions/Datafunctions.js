71174521d8ba187cf88569a2a3870dda225c796e
Petr Halinen <petr.halinen@tuni.fi>
2021-12-29 14:00:15 +0200

    Add datahandling functionality to Datafunctions.js

---
+++ frontend/src/Datafunctions/Datafunctions.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const Datafunctions = {
+  getVocab: async () => {
+    try {
+      let { data } = await axios.get('http://localhost:8080/vocabulary');
+      return data;
+    } catch (error) {
+      return console.log(error);
+    }
+  },
+
+  postNewWord: async (body) => {
+    try {
+      await axios.post('http://localhost:8080/vocabulary', {
+        tags: body.tags,
+        fi_word: body.fi_word,
+        eng_word: body.eng_word,
+        sv_word: body.sv_word,
+        ru_word: body.ru_word,
+      });
+    } catch (error) {
+      return console.log(error);
+    }
+  },
+};
+
+export default Datafunctions;
