306501c14322d1fc2d850020ff143e7b3e55e8a0
Petr Halinen <petr.halinen@tuni.fi>
2022-01-10 16:49:03 +0200

    Add 2 booleans to control if MyTable needs to be reset and if languages have been selected in DropMenu

--- frontend/src/Pages/Main.js
+++ frontend/src/Pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Button } from 'react-bootstrap';
 import DropMenu from '../Components/DropMenu';
 
 import MyTable from '../Components/MyTable';
@@ -7,6 +8,8 @@ import df from '../Datafunctions/Datafunctions';
 function Main(props) {
   const [selectedLangWords, setSelectedLangs] = useState([]);
   const [languages, setLanguages] = useState('');
+  const [selected, setSelected] = useState(false);
+  const [resetTable, setResetTable] = useState(false);
 
   useEffect(() => {
     if (languages !== '') {
@@ -25,8 +28,16 @@ function Main(props) {
       <DropMenu
         selectedLangs={selectedLangWords}
         selectLanguages={selectLanguages}
+        setSelected={setSelected}
+        setResetTable={setResetTable}
       />
-      <MyTable selectedLangWords={selectedLangWords} langs={languages} />
+      <MyTable
+        selectedLangWords={selectedLangWords}
+        langs={languages}
+        selected={selected}
+        resetTable={resetTable}
+        setResetTable={setResetTable}
+      />
     </div>
   );
 }
