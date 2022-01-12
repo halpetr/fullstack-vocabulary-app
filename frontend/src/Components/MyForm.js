0ec116ab478ed939e7c009fef384bf544cd3e60a
Petr Halinen <petr.halinen@tuni.fi>
2022-01-12 12:49:01 +0200

    Move form for deleting a word to own component.

--- frontend/src/Components/MyForm.js
+++ frontend/src/Components/MyForm.js
@@ -1,19 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { Form, Button, Nav, Row, Col, Dropdown } from 'react-bootstrap';
 import df from '../Datafunctions/Datafunctions';
-import Add from './Forms/AddForm';
+import AddForm from './Forms/AddForm';
+import DelForm from './Forms/DelForm';
 
 function MyForm(props) {
   const [langs, setLangs] = useState([]);
   const [dbTags, setDbTags] = useState([]);
-  const [activeLang, setActiveLang] = useState('');
-  const [isLangSelected, setIsLangSelected] = useState(false);
-  const [unUsed, setUnused] = useState([]);
   console.log(props.columns);
   console.log(dbTags);
 
   useEffect(() => {
-    setActiveLang('Select language');
     df.getAllDifferentTags().then((res) => setDbTags(res));
   }, []);
 
@@ -41,73 +38,13 @@ function MyForm(props) {
   // If props.isAdd is true, then return this form and on
   // Add button click post the data to database.
   if (props.isAdd) {
-    return <Add handleClose={handleClose} />;
+    return <AddForm handleClose={handleClose} />;
   }
-
-  const handleDelLangSelect = (language) => {
-    let ls = langs.filter((l) => language !== l);
-    if (ls.length !== langs.length) {
-      setIsLangSelected(true);
-    }
-    setUnused(ls);
-    setActiveLang(language);
-  };
 
   // If props.isDel is true, then return this form
   if (props.isDel) {
     return (
-      <Form id="form">
-        <Form.Label>
-          <h4>
-            Delete by selecting a language and tag or just search for a word:
-          </h4>
-        </Form.Label>
-        <Dropdown onClick={() => getLanguages()}>
-          <Dropdown.Toggle className="mb-2" variant="danger">
-            {activeLang}
-          </Dropdown.Toggle>
-          <Dropdown.Menu variant="dark">
-            {!isLangSelected &&
-              langs.map((index, lang) => {
-                return (
-                  <Dropdown.Item
-                    key={index}
-                    onClick={() => handleDelLangSelect(langs[lang])}
-                  >
-                    {langs[lang]}
-                  </Dropdown.Item>
-                );
-              })}
-            {isLangSelected &&
-              unUsed.map((index, lang) => {
-                return (
-                  <Dropdown.Item
-                    key={index}
-                    onClick={() => handleDelLangSelect(unUsed[lang])}
-                  >
-                    {unUsed[lang]}
-                  </Dropdown.Item>
-                );
-              })}
-          </Dropdown.Menu>
-        </Dropdown>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Control type="text" placeholder="intial" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-        <Button variant="danger">Search</Button>
-
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="danger">Delete</Button>
-      </Form>
+      <DelForm getLanguages={getLanguages} langs={langs} dbTags={dbTags} />
     );
   }
 
