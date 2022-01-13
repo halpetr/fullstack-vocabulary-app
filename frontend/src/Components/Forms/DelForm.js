80013b9b6fb8fed6597e9e2005eb435c10584dc1
Petr Halinen <petr.halinen@tuni.fi>
2022-01-13 15:22:40 +0200

    Import ModForm component and add it to return.

--- frontend/src/Components/Forms/DelForm.js
+++ frontend/src/Components/Forms/DelForm.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Form, Button, Nav, Row, Col, Dropdown, Table } from 'react-bootstrap';
 import df from '../../Datafunctions/Datafunctions';
+import { RiDeleteBin2Line, RiEdit2Line } from 'react-icons/ri';
+import ModForm from './ModForm';
 
 function DelForm(props) {
   const [activeLang, setActiveLang] = useState('');
@@ -10,6 +12,10 @@ function DelForm(props) {
   const [langTags, setLangTags] = useState([]);
   const [showTable, setShowTable] = useState(false);
   const [disableSearch, setDisableSearch] = useState(true);
+  const [showMod, setShowMod] = useState(false);
+  const [clickedWordId, setClickedWordId] = useState(0);
+
+  console.log('showMod', showMod);
 
   useEffect(() => {
     setActiveLang('Select language');
@@ -45,108 +51,137 @@ function DelForm(props) {
     df.deleteById(lt.id).then((res) => handleSearch());
   };
 
-  return (
-    <Form id="form">
-      <Form.Label>
-        <h4>Delete by selecting a language and searching for a word:</h4>
-      </Form.Label>
-      <Dropdown id="dropdown" onClick={() => props.getLanguages()}>
-        <Dropdown.Toggle className="mb-2" variant="danger">
-          {activeLang}
-        </Dropdown.Toggle>
-        <Dropdown.Menu variant="dark">
-          {!isLangSelected &&
-            props.langs.map((index, lang) => {
-              return (
-                <Dropdown.Item
-                  key={index}
-                  onClick={() => handleLangSelect(props.langs[lang])}
-                >
-                  {props.langs[lang]}
-                </Dropdown.Item>
-              );
-            })}
-          {isLangSelected &&
-            unUsedLangs.map((index, lang) => {
-              return (
-                <Dropdown.Item
-                  key={index}
-                  onClick={() => handleLangSelect(unUsedLangs[lang])}
-                >
-                  {unUsedLangs[lang]}
-                </Dropdown.Item>
-              );
-            })}
-        </Dropdown.Menu>
-      </Dropdown>
-      {showTable && (
-        <Table
-          striped
-          bordered
-          hover
-          style={{ maxWidth: '50vw', marginRight: 'auto', marginLeft: 'auto' }}
-        >
-          <thead>
-            <tr style={{ textAlign: 'center' }}>
-              <th>{activeLang}</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            {langTags.map((lt, index) => (
-              <tr key={index}>
-                <td style={{ textAlign: 'center' }}>{lt[activeLang]}</td>
-                <td style={{ textAlign: 'center' }}>
-                  <Button
-                    onClick={() => {
-                      if (
-                        window.confirm(
-                          `Are you sure you wish to delete the word: ${lt[activeLang]}?`
-                        )
-                      ) {
-                        handleDelete(lt);
-                      }
-                    }}
-                    variant="danger"
-                  >
-                    X
-                  </Button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      )}
-      <Form.Group className="mb-3" controlId="search">
-        <Form.Control
-          value={activeSearch}
-          onChange={(e) => handleSearchChange(e)}
-          type="text"
-          placeholder="search"
-        />
-      </Form.Group>
+  const handleModOpen = (id) => {
+    setClickedWordId(id);
+    setShowMod(true);
+  };
 
-      <Row>
-        {isLangSelected && (
+  return (
+    <>
+      <Form id="form">
+        <Form.Label>
+          <h4>Delete by selecting a language and searching for a word:</h4>
+        </Form.Label>
+        <Dropdown id="dropdown" onClick={() => props.getLanguages()}>
+          <Dropdown.Toggle className="mb-2" variant="danger">
+            {activeLang}
+          </Dropdown.Toggle>
+          <Dropdown.Menu variant="dark">
+            {!isLangSelected &&
+              props.langs.map((index, lang) => {
+                return (
+                  <Dropdown.Item
+                    key={index}
+                    onClick={() => handleLangSelect(props.langs[lang])}
+                  >
+                    {props.langs[lang]}
+                  </Dropdown.Item>
+                );
+              })}
+            {isLangSelected &&
+              unUsedLangs.map((index, lang) => {
+                return (
+                  <Dropdown.Item
+                    key={index}
+                    onClick={() => handleLangSelect(unUsedLangs[lang])}
+                  >
+                    {unUsedLangs[lang]}
+                  </Dropdown.Item>
+                );
+              })}
+          </Dropdown.Menu>
+        </Dropdown>
+        {showTable && (
+          <Table
+            striped
+            bordered
+            hover
+            style={{
+              maxWidth: '50vw',
+              marginRight: 'auto',
+              marginLeft: 'auto',
+            }}
+          >
+            <thead>
+              <tr style={{ textAlign: 'center' }}>
+                <th>{activeLang}</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {langTags.map((lt, index) => (
+                <tr key={index}>
+                  <td style={{ textAlign: 'center' }}>{lt[activeLang]}</td>
+                  <td style={{ textAlign: 'center' }}>
+                    <Button
+                      onClick={() => handleModOpen(lt.id)}
+                      size="sm"
+                      variant="info"
+                      id="edit-btn"
+                    >
+                      <RiEdit2Line />
+                    </Button>
+                    <Button
+                      size="sm"
+                      id="del-btn"
+                      onClick={() => {
+                        if (
+                          window.confirm(
+                            `Are you sure you wish to delete the word: ${lt[activeLang]}?`
+                          )
+                        ) {
+                          handleDelete(lt);
+                        }
+                      }}
+                      variant="danger"
+                    >
+                      <RiDeleteBin2Line />
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
+        <Form.Group className="mb-3" controlId="search">
+          <Form.Control
+            value={activeSearch}
+            onChange={(e) => handleSearchChange(e)}
+            type="text"
+            placeholder="search"
+          />
+        </Form.Group>
+
+        <Row>
+          {isLangSelected && (
+            <Col>
+              <Button
+                disabled={disableSearch}
+                variant="danger"
+                onClick={() => handleSearch()}
+              >
+                Search
+              </Button>
+            </Col>
+          )}
+        </Row>
+        <Row className="mt-2">
           <Col>
-            <Button
-              disabled={disableSearch}
-              variant="danger"
-              onClick={() => handleSearch()}
-            >
-              Search
+            <Button onClick={() => props.handleClose()} variant="primary">
+              Go back
             </Button>
           </Col>
-        )}
-      </Row>
-      <Row className="mt-2">
-        <Col>
-          <Button onClick={() => props.handleClose()} variant="primary">
-            Go back
-          </Button>
-        </Col>
-      </Row>
-    </Form>
+        </Row>
+      </Form>
+      {showMod && (
+        <ModForm
+          handleClose={props.handleClose}
+          showMod={showMod}
+          setShowMod={setShowMod}
+          wordId={clickedWordId}
+        />
+      )}
+    </>
   );
 }
 
