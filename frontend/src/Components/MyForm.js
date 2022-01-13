7d38688f3e8edd66f81a33ead2fb23c0fb52ec76
Petr Halinen <petr.halinen@tuni.fi>
2022-01-13 15:26:34 +0200

    Remove ModForm from MyForm

--- frontend/src/Components/MyForm.js
+++ frontend/src/Components/MyForm.js
@@ -32,7 +32,6 @@ function MyForm(props) {
     props.setOptionSelected(false);
     props.setIsAdd(false);
     props.setIsDel(false);
-    props.setIsMod(false);
   };
 
   // If props.isAdd is true, then return this form and on
@@ -50,32 +49,6 @@ function MyForm(props) {
         dbTags={dbTags}
         handleClose={handleClose}
       />
-    );
-  }
-
-  // If props.isDel is true, then return this form
-  if (props.isMod) {
-    return (
-      <Form id="form">
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Modify</Form.Label>
-          <Form.Control type="text" placeholder="intial" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
     );
   }
 }
