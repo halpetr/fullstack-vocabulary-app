060ffc33d621ef550e0a14c758c2398b6a681299
Petr Halinen <petr.halinen@tuni.fi>
2022-01-14 15:33:09 +0200

    Fix child comp props Remove unused imports & vars.

--- frontend/src/Pages/Main.js
+++ frontend/src/Pages/Main.js
@@ -1,39 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { Button } from 'react-bootstrap';
+import React, { useState } from 'react';
 import DropMenu from '../Components/DropMenu';
-
 import MyTable from '../Components/MyTable';
-import df from '../Datafunctions/Datafunctions';
 
 function Main(props) {
-  const [selectedLangWords, setSelectedLangs] = useState([]);
-  const [languages, setLanguages] = useState('');
   const [selected, setSelected] = useState(false);
   const [resetTable, setResetTable] = useState(false);
-
-  useEffect(() => {
-    if (languages !== '') {
-      df.getSelectedLanguages(languages).then((res) => setSelectedLangs(res));
-    }
-  }, [languages]);
 
   const selectLanguages = (sourceLang, targetLang) => {
     let result = sourceLang + '_' + targetLang;
     console.log(result);
-    setLanguages(result);
+    props.setLanguages(result);
   };
 
   return (
     <div id="main">
       <DropMenu
-        selectedLangs={selectedLangWords}
+        selectedLangs={props.selectedLangWords}
         selectLanguages={selectLanguages}
         setSelected={setSelected}
         setResetTable={setResetTable}
+        columns={props.columns}
       />
       <MyTable
-        selectedLangWords={selectedLangWords}
-        langs={languages}
+        selectedLangWords={props.selectedLangWords}
+        langs={props.languages}
         selected={selected}
         resetTable={resetTable}
         setResetTable={setResetTable}
