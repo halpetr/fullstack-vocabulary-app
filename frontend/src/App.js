17bbbd1d33e60db4a69a282c8594f6ffd2826f5b
Petr Halinen <petr.halinen@tuni.fi>
2022-01-15 03:28:12 +0200

    Move functionality down in component hierarchy. It was not needed in App.js.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,22 +8,7 @@ import df from './Datafunctions/Datafunctions';
 import Select from './Pages/Select';
 
 function App() {
-  const [selectedLangWords, setSelectedLangWords] = useState([]);
-  const [languages, setLanguages] = useState('');
   const [databaseCols, setDatabaseCols] = useState([]);
-  const [tag, setTag] = useState('');
-
-  useEffect(() => {
-    if (languages !== '') {
-      if (tag !== 'Select topic') {
-        df.getByTag(tag).then((res) => setSelectedLangWords(res));
-      } else {
-        df.getSelectedLanguages(languages).then((res) =>
-          setSelectedLangWords(res)
-        );
-      }
-    }
-  }, [languages, tag]);
 
   useEffect(() => {
     df.getDatabaseColumns().then((res) => setDatabaseCols(res.data));
@@ -34,19 +19,7 @@ function App() {
       <Navigation />
       <Routes>
         <Route path="*" exact element={<NotFound />} />
-        <Route
-          path="/"
-          exact
-          element={
-            <Main
-              selectedLangWords={selectedLangWords}
-              setLanguages={setLanguages}
-              languages={languages}
-              columns={databaseCols}
-              setTag={setTag}
-            />
-          }
-        />
+        <Route path="/" exact element={<Main columns={databaseCols} />} />
         <Route path="/select" exact element={<Select />} />
         <Route path="/admin" exact element={<Admin columns={databaseCols} />} />
       </Routes>
--- frontend/src/Pages/Main.js
+++ frontend/src/Pages/Main.js
@@ -1,29 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DropMenu from '../Components/DropMenu';
 import MyTable from '../Components/MyTable';
+import df from '../Datafunctions/Datafunctions';
 
 function Main(props) {
   const [selected, setSelected] = useState(false);
   const [resetTable, setResetTable] = useState(false);
+  const [tag, setTag] = useState('');
+  const [selectedLangWords, setSelectedLangWords] = useState([]);
+  const [langs, setLangs] = useState('');
 
-  const setLanguages = (sourceLang, targetLang, tag) => {
+  useEffect(() => {
+    if (langs !== '') {
+      if (tag !== 'Select topic') {
+        df.getByTag(tag).then((res) => setSelectedLangWords(res));
+      } else {
+        df.getWordsOfSelectedLangs(langs).then((res) =>
+          setSelectedLangWords(res)
+        );
+      }
+    }
+  }, [langs, tag]);
+
+  const setLanguages = (sourceLang, targetLang, tagg) => {
     let result = sourceLang + '_' + targetLang;
-    props.setTag(tag);
-    props.setLanguages(result);
+    setTag(tagg);
+    setLangs(result);
   };
 
   return (
     <div id="main">
       <DropMenu
-        selectedLangs={props.selectedLangWords}
+        selectedLangs={selectedLangWords}
         setLanguages={setLanguages}
         setSelected={setSelected}
         setResetTable={setResetTable}
         columns={props.columns}
       />
       <MyTable
-        selectedLangWords={props.selectedLangWords}
-        langs={props.languages}
+        selectedLangWords={selectedLangWords}
+        langs={langs}
         selected={selected}
         resetTable={resetTable}
         setResetTable={setResetTable}
