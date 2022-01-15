c68e5d11653cb995401345a4eb3e579a6f11567d
Petr Halinen <petr.halinen@tuni.fi>
2022-01-15 03:34:06 +0200

    Remove Container. Move css id to another component

--- frontend/src/Navbar/Navigation.js
+++ frontend/src/Navbar/Navigation.js
@@ -1,39 +1,31 @@
 import React, { useState } from 'react';
-import { Nav, Button, Navbar, Container } from 'react-bootstrap';
+import { Nav, Button, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { BiCog } from 'react-icons/bi';
 
 function Navigation(props) {
   return (
     <Navbar collapseOnSelect expand="sm" variant="dark" id="navbar">
-      <Container id="nav-container">
-        <Nav.Link as={Link} to="/">
-          <Navbar.Brand id="navbrand"> Learn Words! </Navbar.Brand>
-        </Nav.Link>
-        <Navbar.Toggle aria-controls="basic-navbar" />
-        <Navbar.Collapse id="basic-navbar">
-          <Nav variant="pills" id="nav">
-            <Nav.Link
-              className="mt-2"
-              id="button"
-              as={Link}
-              to="/"
-              eventKey={1}
-            >
-              Type a word
-            </Nav.Link>
-            <Nav.Link
-              className="mt-2"
-              id="button"
-              as={Link}
-              to="/admin"
-              eventKey={2}
-            >
-              <BiCog style={{ fontSize: '150%' }} />
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+      <Nav.Link id="navbrand" as={Link} to="/">
+        <Navbar.Brand> Learn Words! </Navbar.Brand>
+      </Nav.Link>
+      <Navbar.Toggle aria-controls="basic-navbar" />
+      <Navbar.Collapse id="basic-navbar">
+        <Nav variant="pills" id="nav">
+          <Nav.Link className="mt-2" id="button" as={Link} to="/" eventKey={1}>
+            Type a word
+          </Nav.Link>
+          <Nav.Link
+            className="mt-2"
+            id="button"
+            as={Link}
+            to="/admin"
+            eventKey={2}
+          >
+            <BiCog style={{ fontSize: '150%' }} />
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
